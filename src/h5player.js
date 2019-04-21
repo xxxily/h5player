208c742be330c4e91deaf2a69d7961fb18e4813f
xxxily <974278171@qq.com>
2019-04-21 11:15:48 +0800

    发布2.2.0版本

--- src/h5player.js
+++ src/h5player.js
@@ -2,7 +2,7 @@
 // @name         HTML5播放器增强插件 - 修订版
 // @namespace    https://github.com/xxxily/h5player
 // @homepage     https://github.com/xxxily/h5player
-// @version      2.1.0
+// @version      2.2.0
 // @description  对HTML5播放器的功能进行增强，支持所有使用H5进行视频播放的网站，快捷键仿照Potplayer的快捷键布局，实现调节亮度，饱和度，对比度，速度等功能。
 // @author       ankvps
 // @match        http://*/*
