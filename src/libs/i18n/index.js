889f201dc293208ba23eef254d7cc52455185658
xxxily <974278171@qq.com>
2020-04-23 22:59:15 +0800

    feat:增加i18n方法库

---
+++ src/libs/i18n/index.js
@@ -0,0 +1,25 @@
+/**
+ * 简单的i18n库
+ */
+
+class i18n {
+  constructor (config) {
+    console.log(config)
+  }
+
+  init () {}
+
+  use () {}
+
+  t () {}
+
+  /* 当前语言值 */
+  language () {}
+
+  languages () {}
+
+  changeLanguage () {}
+
+  exists () {}
+}
+export default i18n
