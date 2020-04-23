4fc17847cc782f8bd29f2b7145214393cb270f5c
xxxily <974278171@qq.com>
2020-04-23 23:16:17 +0800

    feat:增加i18n方法库

--- src/libs/I18n/index.js
+++ src/libs/I18n/index.js
@@ -5,21 +5,34 @@
 class I18n {
   constructor (config) {
     console.log(config)
+    this._languages = {}
   }
 
-  init () {}
+  init (config) {
+    this._locale = config.locale
+    this._languages = config.languages
+  }
 
   use () {}
 
   t () {}
 
   /* 当前语言值 */
-  language () {}
+  language () {
+    return this._locale
+  }
 
-  languages () {}
+  languages () {
+    return this._languages
+  }
 
-  changeLanguage () {}
-
-  exists () {}
+  changeLanguage (locale) {
+    if (this._languages[locale]) {
+      this._languages = locale
+      return locale
+    } else {
+      return false
+    }
+  }
 }
 export default I18n
