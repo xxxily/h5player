2492409c3abf69deb604dbb28a1829ab1904fa82
xxxily <974278171@qq.com>
2020-04-26 23:33:09 +0800

    feat:发布3.3.0版本

--- src/h5player/index.js
+++ src/h5player/index.js
@@ -37,7 +37,7 @@ import {
   const i18n = new I18n({
     defaultLanguage: 'en',
     /* 指定当前要是使用的语言环境，默认无需指定，会自动读取 */
-    locale: 'en',
+    // locale: 'zh-TW',
     languages: langMessage
   })
 
