4a1f0cefc056888f6c21374354b0cde190bd5e35
xxxily <974278171@qq.com>
2020-04-30 18:42:27 +0800

    feat:同步代码

--- src/h5player/ui/data.js
+++ src/h5player/ui/data.js
@@ -1,4 +1,22 @@
 const appStatus = {
-  openSettingPanel: true
+  openSettingPanel: true,
+  setting: {
+    /* 在所有页面都运行插件脚本 */
+    initOnPage: 'all',
+    language: 'en',
+    autoPlay: false,
+    defaultSpeed: 1
+  },
+  keyMap: {
+    playspeed: '',
+    playAndPause: '',
+    volumeUp: '',
+    volumeDown: '',
+    mute: '',
+    forward: '',
+    backward: '',
+    nextframe: '',
+    previousframe: ''
+  }
 }
 export default appStatus
