d8429974991b507b387efc13f107e545e2022895
Matej Policky <40162469+Mattes0h@users.noreply.github.com>
2021-07-04 06:04:27 +0200

    Merge remote-tracking branch 'origin/dependabot/npm_and_yarn/browserslist-4.16.6'

--- src/libs/utils/index.js
+++ src/libs/utils/index.js
@@ -4,8 +4,7 @@ import hackAttachShadow from './hackAttachShadow'
 import hackEventListener from './hackEventListener'
 import { getType, isType, isObj, isErr, isArr, isRegExp, isFunction, isUndefined, isNull } from './typeof'
 import { clone, forIn, getObjKeys, mergeObj, merge, getValByPath } from './object'
-import { quickSort } from './number'
-import { hideDom, eachParentNode, getContainer, loadCSSText, isEditableTarget, isInShadow, isInViewPort } from './dom'
+import { hideDom, eachParentNode, getContainer, loadCSSText, isEditableTarget } from './dom'
 import { fakeUA, userAgentMap } from './fakeUA'
 import { fakeVisible, fakeHidden } from './fakeVisibilityState'
 import { isInIframe, isInCrossOriginFrame } from './iframe'
@@ -31,12 +30,9 @@ export {
   mergeObj,
   merge,
   getValByPath,
-  quickSort,
   hideDom,
   loadCSSText,
   isEditableTarget,
-  isInShadow,
-  isInViewPort,
   eachParentNode,
   getContainer,
   fakeUA,
