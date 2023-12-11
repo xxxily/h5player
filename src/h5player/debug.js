95156e469579471ec4d34d4f25c13d130c737ef5
xxxily <974278171@qq.com>
2023-12-11 16:34:36 +0800

    feat: 优化调试方法的底层逻辑

--- src/h5player/debug.js
+++ src/h5player/debug.js
@@ -1,2 +1,7 @@
 import Debug from '../libs/utils/Debug'
-export default Debug.create('h5player message:')
+export default Debug.create({
+  msg: '[H5player Msg]',
+  trace: true,
+  traceGroup: true,
+  printTime: false
+})
